edf0dde5d0c5dcdbc8eb7373a2071c51b6fa5e54
Ana Beatriz Pires <abmpires1999@gmail.com>
2023-05-05 13:16:24 +0100

    pong game is now playable

--- pong-game-script.js
+++ pong-game-script.js
@@ -1,17 +1,7 @@
-
 /* Cria jogo */
+
 var canvas = document.querySelector("canvas")
 var c = canvas.getContext('2d')
-
-c.fillStyle = "transparent"
-c.fillRect(0, 0, 700, 500)
-
-c.setLineDash([30, 20]); /*dashes are 30px and spaces are 20px*/
-c.beginPath();
-c.moveTo(350,10);
-c.lineTo(350, 490);
-c.strokeStyle = "white"
-c.stroke();
 
 class Game {
     constructor(player1, player2, ball) {
@@ -29,17 +19,31 @@ class Game {
     }
 
     addPointsP1() {
+        if(this.pointsP1 + 1 == 10) {
+            this.pointsP1 = 0
+            this.pointsP2 = 0
+        }
+        else {
+            this.pointsP1++
+        }
         c.fillStyle = "#123554"
         c.fillRect(250,10, 50, 50);
-        this.pointsP1++
         c.fillStyle = "white"
         c.fillText(this.pointsP1, 276, 50);
+
+        
     }
 
     addPointsP2() {
+        if(this.pointsP2 + 1 == 10) {
+            this.pointsP1 = 0
+            this.pointsP2 = 0
+        }
+        else {
+            this.pointsP2++
+        }
         c.fillStyle = "#123554"
         c.fillRect(400,10, 50, 50);
-        this.pointsP2++
         c.fillStyle = "white"
         c.fillText(this.pointsP2, 400, 50)
     }
@@ -48,10 +52,16 @@ class Game {
 
 class Player {
     constructor(numPlayer) 
-    {
+    {   
+        if(numPlayer === 1) {
+            this.x = 20
+        }
+        else {
+            this.x = 660
+        }
         this.y = 10;
-        this.height = 500
-        this.width = 100
+        this.height = 75
+        this.width = 20
         this.numPlayer = numPlayer 
     }
 
@@ -60,20 +70,20 @@ class Player {
         c.fillStyle = "white"
 
         if(this.numPlayer === 1) {
-            c.fillRect(20, this.y, 20, 75)
+            c.fillRect(this.x, this.y, this.width, this.height)
         }
         else {
-            c.fillRect(660, this.y, 20, 75)
+            c.fillRect(this.x, this.y, this.width, this.height)
         }
     }
 
     movePlayer(direction) {
 
         if(this.numPlayer === 1) {
-            c.clearRect(20, this.y, 20, 75)
+            c.clearRect(this.x, this.y, 20, 75)
         }
         else {
-            c.clearRect(660, this.y, 20, 75)
+            c.clearRect(this.x, this.y, 20, 75)
         }
         if(direction === 1 && this.y + 4 <= 420 ) {
             this.y += 4
@@ -84,6 +94,24 @@ class Player {
 
         this.drawPlayer()   
     }
+}
+var velocities = [-1, 1];
+class Ball {
+    constructor() {
+        this.height = 20
+        this.width = 20
+        this.radius = 10
+        this.x = 340
+        this.y = 240
+        this.velocity = velocities[Math.floor(Math.random()*velocities.length)];
+        this.direction = Math.random(-1,1);
+    }
+
+    drawBall() {
+        c.fillStyle = "white";
+        c.fillRect(this.x, this.y, 20, 20);
+    }
+
 }
 
 
@@ -99,40 +127,90 @@ addEventListener("keyup", function(event) {
     delete keys[event.keyCode];
 })
 
-class Ball {
-    constructor() {
-        this.height = 10
-        this.width = 10
-        this.velocity = 3
-        this.direction = Math.random(-1, 1)
-    }
-
-    drawBall() {
-        c.fillStyle = "white";
-        c.fillRect(340, 240, 20, 20);
-    }
-
-    moveBall() {
-
-        return true;
-    }
-
-}
-
+//Inicializa coisas do jogo
 
 const player1 = new Player(1)
 const player2 = new Player(2)
-player1.drawPlayer()
-player2.drawPlayer()
-
-const ball = new Ball()
-ball.drawBall()
-
+let ball = new Ball()
 const game = new Game(player1, player2)
-game.drawScoreboard()
+
+function drawAll() {
+
+    c.clearRect(0,0,c.width, c.height)
+
+    c.fillStyle = "#123554"
+    c.fillRect(0, 0, 700, 500)
+
+    c.setLineDash([30, 20]); /*dashes are 30px and spaces are 20px*/
+    c.beginPath();
+    c.moveTo(350,10);
+    c.lineTo(350, 490);
+    c.strokeStyle = "white"
+    c.stroke();
+
+    player1.drawPlayer()
+    player2.drawPlayer()
+    ball.drawBall()
+    game.drawScoreboard()
+
+}
+
+function moveBall() {
+    ballCollision()
+
+   if(ball.y + ball.direction <= 0 || ball.y + ball.direction + ball.height >= canvas.height) {
+    ball.direction = ball.direction * (-1)
+    ball.y += ball.direction
+    ball.x += ball.velocity
+   }
+   else {
+    ball.y += ball.direction
+    ball.x += ball.velocity 
+   }
+
+}
+
+function ballCollision() {
+
+    if( ball.x + ball.velocity <= player1.x && (ball.y + ball.direction >= player1.y || ball.y + ball.direction + ball.height >= player1.y )
+    && (ball.y <= player1.y + player1.height || ball.y + ball.height <= player1.y + player1.height) ) {
+        console.log(ball.y + ball.direction)
+        console.log(player1.y + player1.height)
+        ball.velocity = ball.velocity * -1
+        ball.y += ball.direction
+        ball.x += ball.velocity 
+        ball.velocity += 0.2
+    }
+
+    if(ball.x + ball.velocity >= player2.x && (ball.y + ball.direction >= player2.y || ball.y + ball.direction + ball. height >= player2.y) && 
+    (ball.y <= player2.y + player2.height || ball.y + ball.height <= player2.y + player2.height ) ) {
+        console.log(ball.y + ball.direction)
+        console.log(player1.y + player1.height)
+        ball.velocity = ball.velocity * -1
+        ball.y += ball.direction
+        ball.x += ball.velocity 
+    }
+    
+    if(ball.x + ball.velocity <= 0) {
+        game.addPointsP2()
+        ball = new Ball()
+    }
+    else if(ball.x + ball.velocity + ball.width >= canvas.width){
+        game.addPointsP1()
+        ball = new Ball()
+    }
+    else {
+        ball.y += ball.direction
+        ball.x += ball.velocity
+    }
+
+    drawAll()
+
+}
 
 function game_loop() {
-
+    drawAll()
+    
     if(keys[87] == true) {
         player1.movePlayer(-1)
     }
@@ -146,7 +224,10 @@ function game_loop() {
         player2.movePlayer(1)
     }
 
+    moveBall()
+
     window.requestAnimationFrame(game_loop)
 }
 
 game_loop()
+
